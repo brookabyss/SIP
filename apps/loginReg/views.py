4345a2f7eec25d794867310a385291914ba4b85a
Brook Kebede <brookabyss@gmail.com>
2018-04-26 19:06:11 -0700

    SIP server created and running, initial commit

---
+++ apps/loginReg/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+def index(request):
+    print("index");
+    return render(request,'loginReg/index.html')
+
+# Create your views here.
